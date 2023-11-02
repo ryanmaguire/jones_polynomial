13eb6771f489663c29e285f7a6e31d9f9b6fe77d
ryanmaguire <rmaguire@protonmail.com>
2023-11-02 19:05:01 -0400

    And some more comments.

--- src/circle_counting/normalized_jones.c
+++ src/circle_counting/normalized_jones.c
@@ -16,11 +16,34 @@
  *  You should have received a copy of the GNU General Public License along   *
  *  with jones_polynomial.  If not, see <https://www.gnu.org/licenses/>.      *
  ******************************************************************************/
+
+/*  All typedef's and function prototypes are provided here.                  */
 #include "kauffman.h"
 
+/******************************************************************************
+ *  Functions:                                                                *
+ *      normalized_jones                                                      *
+ *  Purpose:                                                                  *
+ *      Computes the normalized Jones polynomial.                             *
+ *  Arguments:                                                                *
+ *      K (const struct knot *):                                              *
+ *          A knot, represented by extended Gauss code.                       *
+ *  Output:                                                                   *
+ *      J_K (struct laurent_polynomial):                                      *
+ *          The normalized Jones polynomial of K.                             *
+ *  Method:                                                                   *
+ *      Compute the Kauffman bracket [K] and then normalize this by the       *
+ *      writhe. That is:                                                      *
+ *                                                                            *
+ *          J(K)(q) = (-q)^{n_{+} - 2n_{-}} [K](q) / (q + q^{-1})             *
+ *                                                                            *
+ *      where n_{+} is the number of positive crossings in K and n_{-} is the *
+ *      number of negative crossings.                                         *
+ ******************************************************************************/
 struct laurent_polynomial normalized_jones(const struct knot *K)
 {
     struct laurent_polynomial out = normalized_kauffman_bracket(K);
     jones_normalization(K, &out);
     return out;
 }
+/*  End of normalized_jones.                                                  */
