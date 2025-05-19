a9e637b5c4f56887b4131613c62cf59fc1edd42a
Peter Bai <66914473+plastic-bottle@users.noreply.github.com>
2025-05-19 17:29:12 -0400

    Test code for 4_1 and 5_2 knots

--- src/eashoori/main.c
+++ src/eashoori/main.c
@@ -34,7 +34,7 @@ int main() {
     for (int i = 0; i < 6; i++) {
         trefoil.matrix[0][i] = (int*)malloc(3 * sizeof(int));
     }
-    int data[6][3] = {
+    int trefoil_data[6][3] = {
         {1, OVER, 1},  // Crossing one, Over, Positive
         {2, UNDER, 1},  // Crossing two, Under, Positive
         {3, OVER, 1},  // Crossing three, Over, Positive
@@ -44,7 +44,7 @@ int main() {
     };
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < 3; j++) {
-            trefoil.matrix[0][i][j] = data[i][j];
+            trefoil.matrix[0][i][j] = trefoil_data[i][j];
         }
     }
     trefoil.startcross = 1;
@@ -60,5 +60,87 @@ int main() {
     free(trefoil.matrix[0]);
     free(trefoil.matrix);
     free(trefoil.sizecomp);
+
+    struct Gauss fig8;
+    fig8.crossings = 4;
+    fig8.components = 1;
+    fig8.splices = 0;
+    fig8.sizecomp = (int*)malloc(sizeof(int));
+    fig8.sizecomp[0] = 8;
+    fig8.matrix = (int***)malloc(sizeof(int**));
+    fig8.matrix[0] = (int**)malloc(8 * sizeof(int*));
+    for (int i = 0; i < 8; i++) {
+        fig8.matrix[0][i] = (int*)malloc(4 * sizeof(int));
+    }
+    int fig8_data[8][3] = {
+        {1, OVER, -1},  // Crossing one, Over, Positive
+        {4, UNDER, 1},  // Crossing two, Under, Positive
+        {3, OVER, 1},  // Crossing three, Over, Positive
+        {1, UNDER, -1},  // Crossing one again, Under, Positive
+        {2, OVER, -1},  // Crossing two again, Over, Positive
+        {3, UNDER, 1},   // Crossing three again, Under, Positive
+        {4, OVER, 1},  // Crossing one, Over, Positive
+        {2, UNDER, -1}  // Crossing two, Under, Positive
+    };
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 3; j++) {
+            fig8.matrix[0][i][j] = fig8_data[i][j];
+        }
+    }
+    fig8.startcross = 1;
+    fig8.ovorun = OVER;
+    fig8.sign = 1;
+    printf("Jones polynomial of the figure 8 knot: ");
+    struct laurent_polynomial jones_fig8 = JONES(fig8);
+    print_polynomial(&jones_fig8, 'q');
+    for (int i = 0; i < 6; i++) {
+        free(fig8.matrix[0][i]);
+    }
+    free(fig8.matrix[0]);
+    free(fig8.matrix);
+    free(fig8.sizecomp);
+
+    struct Gauss five2;
+    five2.crossings = 5;
+    five2.components = 1;
+    five2.splices = 0;
+    five2.sizecomp = (int*)malloc(sizeof(int));
+    five2.sizecomp[0] = 10;
+    five2.matrix = (int***)malloc(sizeof(int**));
+    five2.matrix[0] = (int**)malloc(10 * sizeof(int*));
+    for (int i = 0; i < 10; i++) {
+        five2.matrix[0][i] = (int*)malloc(5 * sizeof(int));
+    }
+    int five2_data[10][3] = {
+        {1, UNDER, -1},
+        {3, OVER, -1},
+        {4, UNDER, -1},
+        {5, OVER, -1},
+        {2, UNDER, -1},
+        {1, OVER, -1},
+        {5, UNDER, -1},
+        {4, OVER, -1},
+        {3, UNDER, -1},
+        {2, OVER, -1}
+    };
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 3; j++) {
+            five2.matrix[0][i][j] = five2_data[i][j];
+        }
+    }
+    five2.startcross = 1;
+    five2.ovorun = OVER;
+    five2.sign = 1;
+    printf("Jones polynomial of the 5_2 knot: ");
+    struct laurent_polynomial jones_knot = JONES(five2);
+    print_polynomial(&jones_knot, 'q');
+    for (int i = 0; i < 10; i++) {
+        free(five2.matrix[0][i]);
+    }
+    free(five2.matrix[0]);
+    free(five2.matrix);
+    free(five2.sizecomp);
+
+
     return 0;
 }
