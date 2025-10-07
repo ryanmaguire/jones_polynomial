6710557c8e7bc1415d8ae63edef395c143dc038b
Hansen Shieh <cyborgpenguinto@gmail.com>
2025-10-07 17:40:56 -0400

    Adding in knot and crossing structs

---
+++ src/load_balanced/load_balanced.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of jones_polynomial.                                    *
+ *                                                                            *
+ *  jones_polynomial is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  jones_polynomial is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License along   *
+ *
+ *  with jones_polynomial.  If not, see <https://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
+#ifndef LOAD_BALANCED_H
+#define LOAD_BALANCED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Functions to safely use malloc, calloc, and free*/
+extern void* safe_malloc(const size_t size);
+extern void* safe_calloc(const size_t n, const size_t size);
+#define SAFE_FREE(pointer) if (pointer) free(pointer); pointer = NULL
+
+/*Struct for crossing in PD notation; first entry of data is the undercrossing which points at
+the crossing (when the knot is given an orientation), and then lists crossings adjacent to it
+in councterclockwise order */
+struct crossing {
+	int status;
+	struct crossing* data[4];
+};
+
+extern struct crossing* make_crossing(int status, struct crossing** data);
+
+/*Struct for knot in PD notation; contains number of crossings and a pointer to its first 
+crossing */
+struct knot {
+	int number_of_crossings;
+	struct crossing* first_crossing;
+};
+
+
+extern struct knot make_knot(int number_of_crossings, struct crossing* first_crossing);
+#endif
---
+++ src/load_balanced/safe_malloc.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of jones_polynomial.                                    *
+ *                                                                            *
+ *  jones_polynomial is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  jones_polynomial is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License along   *
+ *
+ *  with jones_polynomial.  If not, see <https://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
+ #include "load_balanced.h"
+
+/* Function to safely malloc a pointer with a certain amount of memory */
+void* safe_malloc(const size_t size)
+{
+	/* After allocating the pointer, check that it is not null; if it is, then exit the program */
+	void* allocated_pointer = malloc(size);
+
+	if (allocated_pointer == NULL)
+		exit(EXIT_FAILURE);
+
+	return allocated_pointer;
+}
