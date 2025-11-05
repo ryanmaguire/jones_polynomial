02cb3b8fce719ba19804b3d044321a07e04c0007
Peter Bai <AOEpotato@gmail.com>
2025-11-04 20:07:29 -0500

    Hansen's remaining changes

--- src/load_balanced/null_gamma.c
+++ src/load_balanced/null_gamma.c
@@ -61,6 +61,16 @@ enum boolean null_gamma(struct link* L)
                 struct crossing* very_far_crossing = previous_crossing->data[OPP(next_crossing->ports[OPP(far_index)])];
 
                 int side_crossing_index = 
+                    (current_crossing->data[NEXT(next_index)] == next_crossing) ? 
+                        PREV(next_index) :
+                        NEXT(next_index);
+                struct crossing* side_crossing = current_crossing->data[side_crossing_index];
+
+                int different_side_crossing_index = 
+                    (next_crossing->data[NEXT(far_index)] == current_crossing) ?
+                        PREV(far_index) :
+                        NEXT(far_index);
+                struct crossing* different_side_crossing = next_crossing->data[]
             } else {
                 previous_crossing = current_crossing;
                 current_crossing = current_crossing->data[next_index];
