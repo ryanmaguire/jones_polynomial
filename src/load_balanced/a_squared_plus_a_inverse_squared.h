7b1a62c0b60c49343458a9cfa2959d9b600a9d55
Hansen Shieh <cyborgpenguinto@gmail.com>
2025-11-12 17:47:46 -0500

    Added info for powers of A^2 + A^-2

--- src/load_balanced/a_squared_plus_a_inverse_squared.h
+++ src/load_balanced/a_squared_plus_a_inverse_squared.h
@@ -1,6 +1,26 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of jones_polynomial.                                    *
+ *                                                                            *
+ *  jones_polynomial is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  jones_polynomial is distributed in the hope that it will be useful,       *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License along   *
+ *
+ *  with jones_polynomial.  If not, see <https://www.gnu.org/licenses/>.      *
+ ******************************************************************************/
 #ifndef A_SQUARED_PLUS_A_INVERSE_SQUARED_H
 #define A_SQUARED_PLUS_A_INVERSE_SQUARED_H
 
-
+/* Stores powers of A^2 + A^-2, see definitions file */
+extern int* a_squared_a_inv_squared_powers[];
 
 #endif
